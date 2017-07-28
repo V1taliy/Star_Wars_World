767f858f2e2570053370dbf77157bd73a73086a7
V1taliy <biggone24@gmail.com>
2017-07-28 23:35:16 +0300

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,46 @@
 "use strict";
 
 var gulp = require('gulp'),
+    concat = require('gulp-concat'),
     plumber = require('gulp-plumber'),
+    prefix = require('gulp-autoprefixer'),
+    imagemin = require('gulp-imagemin'),
     browserSync = require('browser-sync').create();
 
+var useref = require('gulp-useref'),
+    gulpif = require('gulp-if'),
+    cssmin = require('gulp-clean-css'),
+    uglify = require('gulp-uglify'),
+    rimraf = require('rimraf'),
+    notify = require('gulp-notify')
 
+
+var paths = {
+  // blocks: 'blocks/',
+    devDir: 'app/',
+   //outputDir: 'build/'
+};
 
 /*********************************
  Developer tasks
  *********************************/
+
+
+//js compile
+gulp.task('scripts', function() {
+    return gulp.src([
+        paths.blocks + '**/*.js',
+        '!' + paths.blocks + '_assets/**/*.js'
+    ])
+        .pipe(concat('main.js'))
+        .pipe(gulp.dest(paths.devDir + 'js/'))
+        .pipe(browserSync.stream());
+});
+
+
+
 //server
-gulp.task('browser-sync', function () {
+gulp.task('browser-sync', function() {
     browserSync.init({
         port: 3000,
         server: {
@@ -18,6 +48,48 @@ gulp.task('browser-sync', function () {
         }
     });
 });
-gulp.task('default', ['browser-sync']);
+
+
+/*********************************
+ Production tasks
+ *********************************/
+//
+// //clean
+// gulp.task('clean', function(cb) {
+//     rimraf(paths.outputDir, cb);
+// });
+//
+// //css + js
+// gulp.task('build', ['clean'], function () {
+//     return gulp.src(paths.devDir + '*.html')
+//         .pipe( useref() )
+//         .pipe( gulpif('*.js', uglify()) )
+//         .pipe( gulpif('*.css', cssmin()) )
+//         .pipe( gulp.dest(paths.outputDir) );
+// });
+//
+// //copy images to outputDir
+// gulp.task('imgBuild', ['clean'], function() {
+//     return gulp.src(paths.devDir + 'img/**/*.*')
+//         .pipe(imagemin())
+//         .pipe(gulp.dest(paths.outputDir + 'img/'));
+// });
+//
+// //copy fonts to outputDir
+// gulp.task('fontsBuild', ['clean'], function() {
+//     return gulp.src(paths.devDir + '/fonts/*')
+//         .pipe(gulp.dest(paths.outputDir + 'fonts/'));
+// });
+
+
+
+
+//default
+gulp.task('default', ['browser-sync', 'scripts']);
+
+// //production
+// gulp.task('prod', ['build', 'imgBuild', 'fontsBuild']);
+
+
 
 
