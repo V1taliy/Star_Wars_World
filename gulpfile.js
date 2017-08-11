2e3464fb81ff210d95d01f6e15f7af105ff2c881
V1taliy <biggone24@gmail.com>
2017-08-11 16:22:15 +0300

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -2,29 +2,15 @@
 
 var gulp = require('gulp'),
     concat = require('gulp-concat'),
-    plumber = require('gulp-plumber'),
-    prefix = require('gulp-autoprefixer'),
-    imagemin = require('gulp-imagemin'),
     browserSync = require('browser-sync').create();
 
-var useref = require('gulp-useref'),
-    gulpif = require('gulp-if'),
-    cssmin = require('gulp-clean-css'),
-    uglify = require('gulp-uglify'),
-    rimraf = require('rimraf'),
-    notify = require('gulp-notify')
-
-
 var paths = {
-  // blocks: 'blocks/',
     devDir: 'app/',
-   //outputDir: 'build/'
-};
+    };
 
 /*********************************
  Developer tasks
  *********************************/
-
 
 //js compile
 gulp.task('scripts', function() {
@@ -37,8 +23,6 @@ gulp.task('scripts', function() {
         .pipe(browserSync.stream());
 });
 
-
-
 //server
 gulp.task('browser-sync', function() {
     browserSync.init({
@@ -49,46 +33,10 @@ gulp.task('browser-sync', function() {
     });
 });
 
-
-/*********************************
- Production tasks
- *********************************/
-//
-// //clean
-// gulp.task('clean', function(cb) {
-//     rimraf(paths.outputDir, cb);
-// });
-//
-// //css + js
-// gulp.task('build', ['clean'], function () {
-//     return gulp.src(paths.devDir + '*.html')
-//         .pipe( useref() )
-//         .pipe( gulpif('*.js', uglify()) )
-//         .pipe( gulpif('*.css', cssmin()) )
-//         .pipe( gulp.dest(paths.outputDir) );
-// });
-//
-// //copy images to outputDir
-// gulp.task('imgBuild', ['clean'], function() {
-//     return gulp.src(paths.devDir + 'img/**/*.*')
-//         .pipe(imagemin())
-//         .pipe(gulp.dest(paths.outputDir + 'img/'));
-// });
-//
-// //copy fonts to outputDir
-// gulp.task('fontsBuild', ['clean'], function() {
-//     return gulp.src(paths.devDir + '/fonts/*')
-//         .pipe(gulp.dest(paths.outputDir + 'fonts/'));
-// });
-
-
-
-
 //default
 gulp.task('default', ['browser-sync', 'scripts']);
 
-// //production
-// gulp.task('prod', ['build', 'imgBuild', 'fontsBuild']);
+
 
 
 
